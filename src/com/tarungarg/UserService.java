eafe32f1ba84db08631e9ecca2dfbae6f8a6f72c
Tarun <tarungarg003@gmail.com>
2018-12-05 18:56:10 +0530

    new methods added

--- src/com/tarungarg/UserService.java
+++ src/com/tarungarg/UserService.java
@@ -1,7 +1,13 @@
 package com.tarungarg;
 
 import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -26,6 +32,49 @@ public class UserService {
 		return userDao.getUser(userid);
 	}
 	
+	@POST
+	@Path("/users")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String createUser(User user){
+		
+		int result = userDao.addUser(user);
+		if(result == 0){
+			return "FAILURE";
+		}
+		return "SUCCESS";
+	}
 	
+	@PUT
+	@Path("/users")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String updateUser(User user){
+		
+		int result = userDao.updateUser(user);
+		if(result == 0){
+			return "FAILURE";
+		}
+		return "SUCCESS";
+	}
+	
+	@DELETE
+	@Path("/users/{userid}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteUser(@PathParam("userid") int userid, User user){
+		
+		int result = userDao.deleteUser(userid);
+		if(result == 0){
+			return "FAILURE";
+		}
+		return "SUCCESS";
+	}
+	
+	@OPTIONS
+	@Path("/users")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getSupportedOperations() {
+		return "GET, PUT, POST, DELETE";
+	}
 	
 }
